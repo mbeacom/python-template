3488f3cd6004e1db39637520393c5a68b211e377
Mark Beacom <7315957+mbeacom@users.noreply.github.com>
2023-07-09 00:11:41 -0400

    Adjust coverage scope and add no argument test case

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,14 +9,20 @@ runner = CliRunner()
 
 
 def test_entry_version_arg() -> None:
-    """Test the entry method with version argument."""
+    """Test the entry method with the version argument."""
     result = runner.invoke(app, ["--version"])
     assert result.exit_code == 0
     assert "python-template version" in result.stdout
 
 
-def test_entry_no_arg() -> None:
-    """Test the entry method with no arguments."""
+def test_entry_help() -> None:
+    """Test the entry method with the help argument."""
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
     assert "OPTIONS" in result.stdout
+
+
+def test_entry_no_arg() -> None:
+    """Test the entry method with no arguments."""
+    result = runner.invoke(app, [])
+    assert result.exit_code == 0
